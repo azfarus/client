0657fba10d62172f641f8c9aca7d40b7a8e38801
Samnun Azfar <samazfar52@gmail.com>
2022-04-21 20:36:03 +0600

    ticketdecor

--- clientFunctions.cpp
+++ clientFunctions.cpp
@@ -622,7 +622,6 @@ start:
 	printf("\t\t\t\t\033[0;36m2)\033[0m Dinner Ticket \033[0;33m( 80 taka )\033[0m\n\n");
 	cout << "\t\t\t\t\033[0;31m>> \033[0m";
 	int choice;
-	cout << " >> ";
 	scanf("%d", &choice);
 
 	if (choice == 1)
